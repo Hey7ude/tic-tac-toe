f7b925c870f731e57044232742518b106325aeb3
mili <m.ahrari77@gmail.com>
2021-03-05 02:04:38 +0330

    def start_game in commandline and gui

--- main.py
+++ main.py
@@ -37,6 +37,30 @@ class Game:
         
 
 
+def start_game_commandline(game1):
+    print('----------')
+    for row in range(game1.demension):
+        test = '| '
+        for col in range(game1.demension):
+            test = test + f'{game1.elements[row][col]} | '
+        print(test)
+        print('----------')
+
+
+
+
+def start_game_gui(game1):
+    import tkinter as tk
+
+
+    window = tk.Tk()
+    for i in range(3):
+        for j in range(3):
+            frame = tk.Frame(master=window, relief = tk.RAISED, borderwidth=5)
+            frame.grid(row=i, column=j)
+            label = tk.Label(master=frame, text=f'{game1.elements[i][j]}')
+            label.pack()
+    window.mainloop()
 
 
 
@@ -45,27 +69,7 @@ player1 = Player('red', 0)
 game1.add_player(player1)
 player2 = Player('blue', 1)
 game1.add_player(player2)
-
-print('----------')
-for row in range(game1.demension):
-    test = '| '
-    for col in range(game1.demension):
-        test = test + f'{game1.elements[row][col]} | '
-    print(test)
-    print('----------')
+start_game_commandline(game1)
+start_game_gui(game1)
 
 
-
-
-
-import tkinter as tk
-
-
-window = tk.Tk()
-for i in range(3):
-    for j in range(3):
-        frame = tk.Frame(master=window, relief = tk.RAISED, borderwidth=5)
-        frame.grid(row=i, column=j)
-        label = tk.Label(master=frame, text=f'{game1.elements[i][j]}')
-        label.pack()
-window.mainloop()
