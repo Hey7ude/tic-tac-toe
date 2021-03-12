e1e721c636804ebbbfa9b3f62bf31de588a13925
mili <m.ahrari77@gmail.com>
2021-03-12 11:35:03 +0330

    our gui almost work fine

--- tk.py
+++ tk.py
@@ -20,21 +20,42 @@ def make_and_get_object(root, object):
     return entry
 
 
+def clicked(pos, game, buttons):
+    game.elements[int(pos[0])][int(pos[1])] = check_turn(game).name
+    buttons[int(pos[0])][int(pos[1])]['text'] = game.elements[int(pos[0])][int(pos[1])]
+    print(game.elements)
+    if game.win_check(pos) == True:
+        print(f'{game.elements[int(pos[0])][int(pos[1])]} won')
+
+
+
 def create_game_start(root, demension, win_on, player_count):
 #create and start game
     buttons = []
     game = Game(demension, win_on, player_count)
     elements = game.elements
     add_players(game)
+    game.players[0].status = True
     top = tk.Toplevel(root)
     for i in range(len(elements)):
         buttons.append([])
         for j in range(len(elements[i])):
             button = tk.Button(top, relief = tk.RAISED, borderwidth=5, text=f'{elements[i][j]}')
-            buttons[i].append(button)
+            button.config(command=lambda current_pos=f'{i}{j}': clicked(current_pos, game, buttons))
             button.grid(row=i,column=j)
-    print(buttons)
+            buttons[i].append(button)
     top.mainloop()
+
+
+def check_turn(game):
+    for i in range(len(game.players)):
+        if game.players[i].status == True:
+            game.players[i].status = False
+            if i == len(game.players)-1:
+                game.players[0].status = True
+            else:
+                game.players[i+1].status = True
+            return game.players[i]
 
 
 def main():
